045a2df60f901f09e09f4867f74a592eed41e05a
edjaper <66147047+edjaper@users.noreply.github.com>
2022-06-21 21:24:43 -0300

    Update mvp1.py

--- mvp1.py
+++ mvp1.py
@@ -36,83 +36,15 @@ explainer1 = lime.lime_tabular.LimeTabularExplainer(np.array(diamonds),
 @st.cache
 
 
-# function that takes in our users’ input (diamond characteristics), and outputs a price prediction
 
-def predict(carat, cut, color, clarity, depth, table, x, y, z):
-    #Predicting the price of the carat
-    if cut == 'Fair':
-        cut = 0
-    elif cut == 'Good':
-        cut = 1
-    elif cut == 'Very Good':
-        cut = 2
-    elif cut == 'Premium':
-        cut = 3
-    elif cut == 'Ideal':
-        cut = 4
-
-    if color == 'J':
-        color = 0
-    elif color == 'I':
-        color = 1
-    elif color == 'H':
-        color = 2
-    elif color == 'G':
-        color = 3
-    elif color == 'F':
-        color = 4
-    elif color == 'E':
-        color = 5
-    elif color == 'D':
-        color = 6
-    
-    if clarity == 'I1':
-        clarity = 0
-    elif clarity == 'SI2':
-        clarity = 1
-    elif clarity == 'SI1':
-        clarity = 2
-    elif clarity == 'VS2':
-        clarity = 3
-    elif clarity == 'VS1':
-        clarity = 4
-    elif clarity == 'VVS2':
-        clarity = 5
-    elif clarity == 'VVS1':
-        clarity = 6
-    elif clarity == 'IF':
-        clarity = 7
-    
-
-    prediction = model.predict(pd.DataFrame([[carat, cut, color, clarity, depth, table, x, y, z]], columns=['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z']))
-    return prediction
-
-st.title('Diamond Price Predictor')
+#st.title('Diamond Price Predictor')
 #st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
 st.image("handLens.PNG")
-st.header('Enter the characteristics of the diamond:')
+st.header('Explanation setup:')
 
 n = st.number_input('Number of features:', min_value=2, max_value=8, value=2)
 
 i = st.number_input('Instance ID:', min_value=0, max_value=100, value=0)
-
-
-cut = st.selectbox('Cut Rating:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-
-color = st.selectbox('Color Rating:', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
-
-clarity = st.selectbox('Clarity Rating:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
-
-
-
-table = st.number_input('Diamond Table Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-
-x = st.number_input('Diamond Length (X) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-
-y = st.number_input('Diamond Width (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-
-z = st.number_input('Diamond Height (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-
 
 
 if st.button('Show explanation'):
