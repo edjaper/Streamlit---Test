9d5c7c7d19a27ce832f58d9a91c5c96e8684d606
edjaper <66147047+edjaper@users.noreply.github.com>
2022-06-21 22:52:21 -0300

    Update mvp1.py

--- mvp1.py
+++ mvp1.py
@@ -110,4 +110,5 @@ if st.button('Show explanation'):
       #st.session_state['feedback'] = st.session_state['feedback'].append(data, ignore_index=True)
       #st.dataframe(st.session_state['feedback'])
       feedback = feedback.append({'id': i, 'feedbacj': txt}, ignore_index=True)
+      st.write(feedback)
       
