08dd531008f3ed5ce0d54e379a503f5d73d6dc40
edjaper <66147047+edjaper@users.noreply.github.com>
2022-06-21 21:29:47 -0300

    Update mvp1.py

--- mvp1.py
+++ mvp1.py
@@ -89,7 +89,7 @@ def predict(carat, cut, color, clarity, depth, table, x, y, z):
 #st.title('Diamond Price Predictor')
 #st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
 st.image("handLens.PNG")
-st.header('Explanation setup:')
+st.markdown('Explanation setup:')
 
 n = st.number_input('Number of features:', min_value=2, max_value=8, value=2)
 
