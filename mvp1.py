6d90db4b712dd9e8adb9e58aa79853708885e4e1
edjaper <66147047+edjaper@users.noreply.github.com>
2022-06-22 01:36:34 -0300

    Update mvp1.py

--- mvp1.py
+++ mvp1.py
@@ -102,6 +102,8 @@ if st.button('Show explanation'):
     st.pyplot(explanation.as_pyplot_figure())
     txt = st.text_area('Feedback')
     if st.button('Submit'):
+      feedback = feedback.append({'id': i, 'feedback': txt}, ignore_index=True)
+      st.write(feedback)
       st.session_state.feedback = st.session_state.feedback.append({"id": [i], "feedback": [txt]}, ignore_index=True)
       st.dataframe(session_state.feedback)
 
